bb478990544485abc412e911ebc911ae0534399a
Gavin <liugf_99@163.com>
2019-10-28 23:16:08 -0400

    concurrent test

--- test/container/hash_table_test.cpp
+++ test/container/hash_table_test.cpp
@@ -116,6 +116,7 @@ namespace bustub {
 
     // insert a few values
     for (int i = 0; i < 10000; i++) {
+      LOG_DEBUG("Resize Insert: %d\n", i);
       ht.Insert(nullptr, i, i);
       std::vector<int> res;
       ht.GetValue(nullptr, i, &res);
