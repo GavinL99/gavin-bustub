ad982b9fcdbcb34d5f9b8f4b93d4775b8eb5c3c4
Gavin <liugf_99@163.com>
2019-11-07 17:45:37 -0500

    catalog add test cases

--- src/include/catalog/simple_catalog.h
+++ src/include/catalog/simple_catalog.h
@@ -58,7 +58,7 @@ class SimpleCatalog {
     auto table = std::make_unique<TableHeap>(bpm_, lock_manager_, log_manager_, txn);
     auto output = std::make_unique<TableMetadata>(schema, table_name, std::move(table), next_table_oid_);
     table_oid_t t_id = next_table_oid_++;
-    LOG_DEBUG("create: %d\n", t_id);
+    LOG_DEBUG("create: %d\n", tables_.size());
     tables_[t_id] = std::move(output);
     names_.insert({table_name, t_id});
     return output.get();
