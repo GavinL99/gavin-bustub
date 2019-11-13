a019eb51552c2d1b1235d942b1ffe284aa42fe18
Gavin <liugf_99@163.com>
2019-11-12 20:00:45 -0500

    finish tmp tuple

--- test/storage/tmp_tuple_page_test.cpp
+++ test/storage/tmp_tuple_page_test.cpp
@@ -48,6 +48,15 @@ TEST(TmpTuplePageTest, BasicTest) {
   ASSERT_EQ(*reinterpret_cast<uint32_t *>(data + PAGE_SIZE - 4), 123);
   ASSERT_EQ(tmp_tuple.GetPageId(), page_id);
   ASSERT_EQ(tmp_tuple.GetOffset(), PAGE_SIZE - 4);
+
+  Tuple tuple1(values, &schema);
+  page.Insert(tuple, &tmp_tuple);
+
+  ASSERT_EQ(*reinterpret_cast<uint32_t *>(data + sizeof(page_id_t) + sizeof(lsn_t)), PAGE_SIZE - 16);
+  ASSERT_EQ(*reinterpret_cast<uint32_t *>(data + PAGE_SIZE - 16), 4);
+  ASSERT_EQ(*reinterpret_cast<uint32_t *>(data + PAGE_SIZE - 12), 123);
+  ASSERT_EQ(tmp_tuple.GetPageId(), page_id);
+  ASSERT_EQ(tmp_tuple.GetOffset(), PAGE_SIZE - 12);
 }
 
 }  // namespace bustub
