6c90adf80b4ba1990b31c547b77d9367d7fe77bd
GavinL99 <liugf_99@163.com>
2019-12-10 20:58:47 -0500

    add multi txn test

--- src/recovery/log_recovery.cpp
+++ src/recovery/log_recovery.cpp
@@ -127,7 +127,7 @@ namespace bustub {
     lsn_t temp_lsn = temp_log.lsn_;
     txn_id_t temp_txn = temp_log.txn_id_;
     LogRecordType temp_type = temp_log.log_record_type_;
-//    LOG_DEBUG("Replay: %s\n", temp_log.ToString().c_str());
+    LOG_DEBUG("Replay: %s\n", temp_log.ToString().c_str());
 
     if (temp_type == LogRecordType::COMMIT || temp_type == LogRecordType::ABORT) {
       active_txn_.erase(temp_txn);
@@ -257,7 +257,7 @@ namespace bustub {
           LOG_ERROR("Fail to deserialize log...\n");
           break;
         }
-//        LOG_DEBUG("Undo: LSN: %d, actual: %d, map: %d\n", temp_lsn, temp_log.lsn_, temp_lsn);
+        LOG_DEBUG("Undo: LSN: %d, actual: %d, map: %d\n", temp_lsn, temp_log.lsn_, temp_lsn);
         assert(temp_log.lsn_ == temp_lsn);
         LogRecordType temp_type = temp_log.log_record_type_;
 
